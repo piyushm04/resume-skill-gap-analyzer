6e611431707040c421b5fefeccca78ca01bd1861
piyushm04 <152985829+piyushm04@users.noreply.github.com>
2025-07-28 23:36:27 +0530

    Update skill_extractor.py

--- skill_extractor.py
+++ skill_extractor.py
@@ -1,11 +1,10 @@
-# utils/skill_extractor.py
 import re
 
-def extract_skills(text, skill_set):
+def extract_skills_from_resume(text, skill_set):
     text = text.lower()
-    extracted = set()
+    found_skills = set()
     for skill in skill_set:
         pattern = r'\b' + re.escape(skill.lower()) + r'\b'
         if re.search(pattern, text):
-            extracted.add(skill)
-    return extracted
+            found_skills.add(skill.lower())
+    return found_skills
